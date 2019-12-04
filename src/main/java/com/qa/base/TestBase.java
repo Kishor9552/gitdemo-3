2759fc6036405f5a5a95f1eb27b2439caea5aefa
Kishor9552 <kishorsapkal18@gmail.com>
2019-12-04 15:55:19 +0530

    commiting Git Demo2

---
+++ src/main/java/com/qa/base/TestBase.java
@@ -0,0 +1,8 @@
+package com.qa.base;
+
+public class TestBase {
+	public void Initilisation() {
+		System.out.println("test base");
+	}
+
+}
---
+++ src/test/java/com/qa/test/Logintest.java
@@ -0,0 +1,8 @@
+package com.qa.test;
+
+public class Logintest {
+	public void loginpageTest() {
+		System.out.println("loginpageTest");
+	}
+
+}
